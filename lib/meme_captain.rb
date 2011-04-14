5b688b429351c4b4465babd0c133acafea2f2fc8
Matthew M. Boedicker <matthewm@boedicker.org>
2011-04-13 22:11:05 -0400

    initial

---
+++ lib/meme_captain.rb
@@ -0,0 +1,63 @@
+require 'RMagick'
+
+module MemeCaptain
+
+  module Draw
+
+    # Set the largest point size so that text will fit in max_x, max_y.
+    def set_point_size(text, max_x, max_y, min_pointsize=24)
+      cur_pointsize = min_pointsize
+      self.pointsize = cur_pointsize
+      metrics = get_multiline_type_metrics(text)
+
+      while metrics.width < max_x and metrics.height < max_y
+        cur_pointsize += 1
+        self.pointsize = cur_pointsize
+        metrics = get_multiline_type_metrics(text)
+      end
+
+      self.pointsize = cur_pointsize - 1
+    end
+
+  end
+
+  module_function
+
+  def meme(path_or_io, line1, line2)
+    img = Magick::ImageList.new(path_or_io)
+
+    text1 = Magick::Draw.new {
+      self.fill = 'white'
+      self.font = 'Helvetica'
+      self.stroke = 'black'
+      self.stroke_width = 2
+    }
+    text1.extend(MemeCaptain::Draw)
+
+    text1.set_point_size(line1, img.columns, img.rows / 4)
+
+    text1.annotate(img, 0, 0, 0, 0, line1) {
+      self.gravity = Magick::NorthGravity
+    }
+
+    text2 = Magick::Draw.new {
+      self.fill = 'white'
+      self.font = 'Helvetica'
+      self.stroke = 'black'
+      self.stroke_width = 2
+    }
+    text2.extend(MemeCaptain::Draw)
+
+    text2.set_point_size(line2, img.columns, img.rows / 4)
+
+    text2.annotate(img, 0, 0, 0, 0, line2) {
+      self.gravity = Magick::SouthGravity
+    }
+
+    img
+  end
+
+end
+
+i = MemeCaptain::meme('luke.jpeg', 'meme test', 'meme generator test')
+i.display
