c7b87ce43b5878ba752c45dd4522699ea2fc2d35
Matthew M. Boedicker <matthewm@boedicker.org>
2011-11-23 14:45:00 -0500

    allow most parameters to be passed in as optional arguments

--- lib/meme_captain/meme.rb
+++ lib/meme_captain/meme.rb
@@ -5,7 +5,19 @@ module MemeCaptain
   module_function
 
   # Create a meme image.
+  #
   # Input can be an IO object or a blob of data.
+  #
+  # Options:
+  #   max_lines - maximum number of text lines per caption
+  #   min_pointsize - minimum point size
+  #   super_sample - work this many times larger before shrinking
+  #   text_height_pct - float percentage of image height for each caption
+  #   text_width_pct - float percentage of image width for each caption
+  #
+  #  Any other options will be set on the Draw objects for the text and
+  #  can be used to control text fill, stroke, etc. See RMagick annotate
+  #  attributes.
   def meme(input, top_text, bottom_text, options={})
     img = Magick::ImageList.new
     if input.respond_to?(:read)
@@ -14,23 +26,39 @@ module MemeCaptain
       img.from_blob(input)
     end
 
-    max_lines = 16
-    super_sample = 2.0
+    options = {
+      :max_lines       => 16,
+      :min_pointsize   => 12,
+      :super_sample    => 2.0,
+      :text_height_pct => 0.25,
+      :text_width_pct  => 0.9,
 
-    min_pointsize = 12 * super_sample
-    text_width = img.page.width * 0.9 * super_sample
-    text_height = img.page.height / 4.0 * super_sample
+      # Draw options
+      :fill            => 'white',
+      :font            => 'Impact',
+      :stroke          => 'black',
+      :stroke_width    => 8,
+    }.merge(options)
+
+    max_lines = options.delete(:max_lines)
+    super_sample = options.delete(:super_sample)
+    min_pointsize = options.delete(:min_pointsize) * super_sample
+
+    text_width = img.page.width * options.delete(:text_width_pct) * super_sample
+    text_height = img.page.height * options.delete(:text_height_pct) *
+      super_sample
 
     text_layer = Magick::Image.new(
       img.page.width * super_sample, img.page.height * super_sample) {
       self.background_color = 'none'
-      self.density = 144
+      self.density = 72.0 * super_sample
     }
 
-    draw = Magick::Draw.new.extend(Draw)
+    draw = Magick::Draw.new {
+      options.each { |k,v| self.send("#{k}=", v) }
+    }
 
-    draw.fill = 'white'
-    draw.font = 'Impact'
+    draw.extend(Draw)
 
     [
       [Caption.new(top_text), Magick::NorthGravity],
@@ -52,15 +80,14 @@ module MemeCaptain
       draw.gravity = gravity
       draw.pointsize = choice.pointsize
 
-      draw.stroke = 'black'
-      draw.stroke_width = 8
+      draw.stroke = options[:stroke]
       draw.annotate text_layer, 0, 0, 0, 0, choice.text
 
       draw.stroke = 'none'
       draw.annotate text_layer, 0, 0, 0, 0, choice.text
     end
 
-    text_layer.resize!(1 / super_sample)
+    text_layer.resize!(1.0 / super_sample)
 
     img.each do |frame|
       frame.composite!(text_layer, -frame.page.x, -frame.page.y,
