44df62df3115c4b8b96897e7b95b0f22fee30deb
Matthew M. Boedicker <matthewm@boedicker.org>
2011-11-29 21:05:29 -0500

    define source images in json and load with ajax

--- script/thumb_sprites.rb
+++ script/thumb_sprites.rb
@@ -1,20 +1,33 @@
+require 'json'
 require 'RMagick'
 
 # make a combined thumbnail image from a list of images for use in CSS sprites
+# generate source images description json
 
-puts 'images = ['
+url_prefix = 'http://memecaptain.com/'
+
+data = {
+  :thumbHeight => 50,
+  :thumbSpritesUrl => "#{url_prefix}thumbs.jpg",
+  :images => [],
+}
 
 thumbs = Magick::ImageList.new
 
 ARGV.sort.each do |file|
   image = Magick::ImageList.new(file)
-  image.resize_to_fit!(0, 50)
+  image.resize_to_fit!(0, data[:thumbHeight])
   image.each do |frame|
     thumbs.push frame
-    puts "  ['#{File.basename(file)}', #{frame.columns}],"
+    data[:images] << {
+      :url => "#{url_prefix}#{File.basename(file)}",
+      :thumbWidth => frame.columns
+    }
   end
 end
 
-puts ']'
+open('source_images.json', 'w') do |f|
+  f.write(JSON.pretty_generate(data))
+end
 
 thumbs.append(false).write('thumbs.jpg')
