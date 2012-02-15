e34b4974ef43a3f95fa1e174caf3fd1de8a48f1c
Matthew M. Boedicker <matthewm@boedicker.org>
2012-02-14 23:22:21 -0500

    do not display image, check sha1 sum

--- spec/meme_captain_spec.rb
+++ spec/meme_captain_spec.rb
@@ -1,4 +1,6 @@
+require 'digest/sha1'
 require 'open-uri'
+require 'tempfile'
 
 require 'meme_captain'
 
@@ -7,7 +9,12 @@ describe MemeCaptain, '.meme_top_bottom' do
   it 'generates a meme image with text at the top and bottom' do
     i = MemeCaptain.meme_top_bottom(
       open('http://memecaptain.com/troll_face.jpg'), 'top text', 'bottom text')
-    i.display
+    path = Tempfile.new('meme_captain_test').path
+    i.write path
+    open(path) do |f|
+      Digest::SHA1.hexdigest(f.read).should ==
+        '59b57d624d2cd6f0e10e1b1e25221673c192b602'
+    end
   end
 
 end
