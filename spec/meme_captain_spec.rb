011b1ca395bb6a9417d32f7f3f05f42f2e273947
Matthew M. Boedicker <matthewm@boedicker.org>
2015-04-24 23:37:58 -0700

    Don't require Impact font in spec.

--- spec/meme_captain_spec.rb
+++ spec/meme_captain_spec.rb
@@ -12,8 +12,10 @@ describe MemeCaptain, '.meme_top_bottom' do
       :body => Base64.decode64(
         'iVBORw0KGgoAAAANSUhEUgAAAcwAAAAyAgMAAACsWgPIAAAAAXNSR0IArs4c6QAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAd0SU1FB9wBHwQoJY1iyrwAAAAJUExURQAAAAAAAP///4Pdz9IAAAABdFJOUwBA5thmAAAAAWJLR0QB/wIt3gAAAHpJREFUWIXt1zESgCAMRNE03s9mG+9ns6e0EaKCqDOWf4sUJOHREvqciOn7Us0cEZiYmJhnc7HtVbJtS5LsVRo09DScZzmSG5iYmJit2RTVlduGquS920hFvxRMTEzMRzOv6TfKheMX9ThMTEzMnvlj5ld/S0xMTMxjNoGjc3pdi6L4AAAAAElFTkSuQmCC'))
 
+    font = File.expand_path('../assets/Coda-Heavy.ttf', __FILE__)
     i = MemeCaptain.meme_top_bottom(
-      open('http://memecaptain.com/good.jpg'), 'top text', 'bottom text')
+      open('http://memecaptain.com/good.jpg'), 'top text', 'bottom text',
+        font: font)
   end
 
 end
